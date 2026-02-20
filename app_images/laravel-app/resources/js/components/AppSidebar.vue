337fc8cf25269ada0a0b2f6033782a74dba985b7
cris <crisjohnreytarpin@gmail.com>
2026-02-20 22:47:10 +0000

    updates

--- app_images/laravel-app/resources/js/components/AppSidebar.vue
+++ app_images/laravel-app/resources/js/components/AppSidebar.vue
@@ -13,7 +13,8 @@ import {
     SidebarMenuButton,
     SidebarMenuItem,
 } from '@/components/ui/sidebar';
-import { dashboard, tasks } from '@/routes';
+import { dashboard } from '@/routes';
+import tasks from '@/routes/tasks';
 import { type NavItem } from '@/types';
 import AppLogo from './AppLogo.vue';
 
